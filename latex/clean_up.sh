6bdd037b2b1dc503ae1feb63a07bd5c38822546e
Nick Heppert <nick.heppert@gmx.de>
2018-10-07 20:34:37 +0900

    Finish Pre-publish version of proposal

--- latex/clean_up.sh
+++ latex/clean_up.sh
@@ -2,7 +2,7 @@
 # Pass folder directory to the script
 shopt -s extglob nocaseglob
 cd $1
-for file in !(@(*.sty|*.pdf|*.tex|*.sh|*.txss|*.xmpdata|*.bib|*.cls|*.bst|*.sty|*.eps)); do
+for file in !(@(*.sty|*.pdf|*.tex|*.sh|*.txss|*.xmpdata|*.bib|*.cls|*.bst|*.sty|*.eps|*.png)); do
     [[ -f "${file}" ]] && files+=( "${file}" )
 done
 (( ${#files[@]} )) && rm "${files[@]}" 
